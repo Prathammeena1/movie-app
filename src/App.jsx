af570d7effcf1f9b160c74f9f5f57f51f4516630
pratham meena <yashmeena274@gmail.com>
2024-07-15 12:20:27 +0530

    routing done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen w-full bg-[#2c3e50] ' >
+      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+
+    </div>
   )
 }
 
